107ca564e575cccdf854485cd67594de7049bb59
DhanyashreeNarayani <dhanya2609@gmail.com>
2018-04-12 00:24:30 +0530

    message1

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request').defaults({encoding: null});
-const API_AI_TOKEN = 'y0fba48aabda74cefbb84bc7b8ec64056';
-const apiAiClient = require('apiai')(API_AI_TOKEN);
 
 const app = express();
 
@@ -53,14 +51,7 @@ app.post('/webhook/',(req,res)=>{
             }
         }
         if(event.message && event.message.text){
-            const apiaiSession = apiAiClient.textRequest(event.message.text, {sessionId: '12345'});
-            apiaiSession.on('response', (response) => {
-                result = response.result.fulfillment.speech;
-                sendText(sender,result);
-            });
-            apiaiSession.on('error', error => console.log(error));
-            apiaiSession.end();
-            /*	var apiai=require('apiai');
+           	var apiai=require('apiai');
             var appi = apiai("43a0d573c5564e62bf4d8d38100a2e23");
             var requa=appi.textRequest(event.message.text,{
                 sessionId: '12345'
@@ -79,7 +70,7 @@ app.post('/webhook/',(req,res)=>{
             requa.on('error', function(error) {
                 //res.send(mine['fulfillment'].speech) ;
                    console.log(error);
-            });*/
+            });
            
             /*let text = event.message.text;
            if(text=='hi'||text=='Hi'||text=='Hello')
